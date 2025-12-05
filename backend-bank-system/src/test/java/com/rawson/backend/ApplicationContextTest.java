0422f3a585a7463349f9a051493f11237a0c4e60
fevece2006 <fevece2006@gmail.com>
2025-12-05 13:28:05 -0500

    Create ApplicationContextTest.java

---
+++ backend-bank-system/src/test/java/com/rawson/backend/ApplicationContextTest.java
@@ -0,0 +1,13 @@
+package com.rawson.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationContextTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
